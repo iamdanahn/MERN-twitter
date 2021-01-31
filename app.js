3a83e9c0db6014b710d89d863857dba26b0386c3
Daniel Ahn <iamdanahn@gmail.com>
2021-01-31 00:04:18 -0500

    Connect to MongoDB

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express")
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
+const db = require("./config/keys.js").mongoURI;
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-const db = require("./config/keys").mongoURI;
-const mongoose = require('mongoose')
 
 mongoose
 	.connect(db, { useNewUrlParser: true })
 	.then(() => console.log("Connected to MongoDB successfully"))
 	.catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Get in ma bellay"));
+app.get("/", (req, res) => {
+	res.send("Get in ma bellay")
+});
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
