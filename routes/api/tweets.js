974b4f0b25922584182b2011a1a02d5219245140
Daniel Ahn <iamdanahn@gmail.com>
2021-02-01 00:18:25 -0500

    Setup frontend

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -6,15 +6,15 @@ const passport = require('passport');
 const Tweet = require('../../models/Tweet');
 const validateTweetInput = require('../../validation/tweets');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // finds all tweets
     Tweet.find()
-        .sort({ date: -1 })
-        .then(tweets => res.json(tweets))
+        .sort({ date: -1 }) // newest first
+        .then(tweets => res.json(tweets)) // renders tweet in json
         .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
 });
 
-router.get('/user/:user_id', (req, res) => {
-    Tweet.find({user: req.params.user_id})
+router.get('/user/:user_id', (req, res) => { 
+    Tweet.find({user: req.params.user_id}) // searches all tweets of user
         .then(tweets => res.json(tweets))
         .catch(err =>
             res.status(404).json({ notweetsfound: 'No tweets found from that user' }
@@ -22,7 +22,7 @@ router.get('/user/:user_id', (req, res) => {
     );
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => { // finds tweet by id
     Tweet.findById(req.params.id)
         .then(tweet => res.json(tweet))
         .catch(err =>
@@ -33,7 +33,7 @@ router.get('/:id', (req, res) => {
     });
 router.post('/',
     // adds user on request, passport is middleware fn here
-    passport.authenticate('jwt', { session: false }),
+    passport.authenticate('jwt', { session: false }), // authenticates which user is logged in
     (req, res) => {
       const { errors, isValid } = validateTweetInput(req.body); //validates tweet structure
   
@@ -41,7 +41,7 @@ router.post('/',
         return res.status(400).json(errors);
       }
   
-      const newTweet = new Tweet({
+      const newTweet = new Tweet({ // imported from ln6
         text: req.body.text,
         user: req.user.id
       });
